caa377a9f31034f4731924d9a21b81d17ad508f7
agallego <gallego.alexx@gmail.com>
2015-07-28 15:57:10 -0400

    making cmd publishable in pypi

--- setup.py
+++ setup.py
@@ -6,21 +6,22 @@ from pip.req import parse_requirements
 from subprocess import call
 
 
-install_reqs = parse_requirements("requirements.txt",
-                                  session=pip.download.PipSession())
-# reqs is a list of requirement
+
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+reqs = [
+    'graphviz==0.4.3',
+    'kazoo==2.0',
+    'thrift==0.9.2',
+    'trace2html==0.2.1'
+]
 
 
-#thrift -out generated --gen  py:json,utf8string  concord/concord/thrift/bolt.thrift"
-
-setup(version='0.1',
+setup(version='0.1.0',
       name='concord',
       description='python concord command line tools',
       scripts=['concord'],
       author='concord systems',
-      author_email='hello@rbonut.io',
+      author_email='hello@concord.io',
       packages=find_packages('.'),
       url='http://concord.io',
       install_requires=reqs,
